7d5fee7b87ed0a8dd053ae03eca87e4f025b105f
Yoni190 <yfisuhu@gmail.com>
2024-08-08 12:11:23 +0300

    Create functions for the four operations

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;
+}
+console.log(add(3,5));
+console.log(subtract(3,5));
+console.log(multiply(3,5));
+console.log(divide(3,5));
